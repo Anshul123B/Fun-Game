08b1ebfa5a75578b949566ebd1c1ba65649e9124
Anshul Bhatt <anb201982@gmail.com>
2026-05-01 12:50:38 +0530

    Remove unused backend directory completely

--- backend/routes/scores.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getScores, addScore } = require('../controllers/scoreController');
-
-router.get('/', getScores);
-router.post('/', addScore);
-
-module.exports = router;
